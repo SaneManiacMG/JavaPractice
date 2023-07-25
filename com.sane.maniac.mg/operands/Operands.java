a15af0097f6dfc1ed6b83ee41c5382b62ce65659
SaneManiacMG <mmoroane@hotmail.com>
2023-07-25 15:47:15 +0200

    Added methods for logicalOperators

---
+++ com.sane.maniac.mg/operands/Operands.java
@@ -0,0 +1,27 @@
+package com.sane.maniac.mg.operands;
+
+public abstract class Operands {
+    public static void arithmeticOperators() {
+
+    }
+
+    public static void relationalOperators() {
+
+    }
+
+    public static void logicalOperators() {
+
+    }
+
+    public static void assignmentOperators() {
+
+    }
+
+    public static void incrementAndDecrementOperators() {
+
+    }
+
+    public static void ternaryOperators() {
+
+    }
+}
