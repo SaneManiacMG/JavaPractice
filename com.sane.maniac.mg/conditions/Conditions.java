8d9d7130d5ec6b324996c4515ffdca7a2eec80eb
SaneManiacMG <mmoroane@hotmail.com>
2023-07-26 10:40:58 +0200

    Added switch case

--- com.sane.maniac.mg/conditions/Conditions.java
+++ com.sane.maniac.mg/conditions/Conditions.java
@@ -31,8 +31,27 @@ public class Conditions {
         System.out.println("================================\n");
     }
 
+    public static void switchCaseStatements() {
+        System.out.println("Switch-Case Statements:");
+        System.out.println("switch ([expression]) {\n\tcase [value]:\n\t\t[statements]\n\t\tbreak;\n\tcase [value]:\n\t\t[statements]\n\t\tbreak;\n\tdefault:\n\t\t[statements]\n}");
+        System.out.println("int a = 10;\nswitch (a) {\n\tcase 10:\n\t\tSystem.out.println(\"A is 10\");\n\t\tbreak;\n\tcase 20:\n\t\tSystem.out.println(\"A is 20\");\n\t\tbreak;\n\tdefault:\n\t\tSystem.out.println(\"A is neither 10 nor 20\");\n}");
+        int a = 10;
+        switch (a) {
+            case 10:
+                System.out.println("A is 10");
+                break;
+            case 20:
+                System.out.println("A is 20");
+                break;
+            default:
+                System.out.println("A is neither 10 nor 20");
+        }
+        System.out.println("================================\n");
+    }
+
     public static void callAllMethods() {
         ifElse();
         elseIfConditions();
+        switchCaseStatements();
     }
 }
