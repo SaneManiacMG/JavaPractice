a68d497897934b95b87b89017d72286d1d70c92b
SaneManiacMG <mmoroane@hotmail.com>
2023-08-03 15:02:01 +0200

    Added Lists to Collections class

--- com.sane.maniac.mg/genericsandcollections/Collections.java
+++ com.sane.maniac.mg/genericsandcollections/Collections.java
@@ -1,11 +1,14 @@
 package com.sane.maniac.mg.genericsandcollections;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Collections {
     public static void main(String[] args) {
         arrays();
+        lists();
     }
 
     public static void arrays() {
@@ -19,6 +22,27 @@ public class Collections {
         System.out.println("Copy of Array:\t\t\t\t" + Arrays.toString(copyOfNames));
         System.out.println("Array Sorting: (Arrays.sort(names))" + Arrays.toString(names));
         System.out.println("Array Binary Search: (Arrays.binarySearch(names, \"Jack\"))\t" + Arrays.binarySearch(names, "Jack"));
+        System.out.println();
     }
 
+    public static void lists() {
+        List<String> names = new ArrayList<>();
+        names.add("John");
+        names.add("Jane");
+        names.add("Jack");
+        names.add("Jill");
+
+        System.out.println("List:\t\t\t\t\t" + names);
+        System.out.println("List Size: (names.size())\t" + names.size());
+        System.out.println("List Element: (names.get(0))\t" + names.get(0));
+        System.out.println("List Contains: (names.contains(\"Jack\"))\t" + names.contains("Jack"));
+        System.out.println("List Index: (names.indexOf(\"Jack\"))\t" + names.indexOf("Jack"));
+        System.out.println("List Remove: (names.remove(\"Jack\"))\t" + names.remove("Jack"));
+        System.out.println("List:\t\t\t\t\t" + names);
+        System.out.println("List Remove: (names.remove(0))\t" + names.remove(0));
+        System.out.println("List:\t\t\t\t\t" + names);
+        System.out.println("List set: (names.set(0, \"Jack\"))\t" + names.set(0, "Joemama"));
+        System.out.println("List:\t\t\t\t\t" + names);
+        System.out.println();
+    }
 }
