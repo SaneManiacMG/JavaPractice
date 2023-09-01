b340df7251958ff803b0b0e9513bf01ad2abb1ad
SaneManiacMG <mmoroane@hotmail.com>
2023-09-01 10:35:13 +0200

    Created ThreadsApp to see basic threads working, Tried to create modules

---
+++ com.sane.maniac.mg/javaplatformmodulesystems/DummyMain.java
@@ -0,0 +1,12 @@
+package com.sane.maniac.mg.javaplatformmodulesystems;
+
+public class DummyMain {
+
+  public static void main(String[] args) {
+    System.out.println(hello("MG"));
+  }
+
+  public static String hello(String name) {
+    return "Hello " + name + "!";
+  }
+}
