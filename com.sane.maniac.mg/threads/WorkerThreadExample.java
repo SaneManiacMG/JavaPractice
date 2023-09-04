7df33ab9ea16fb94d82eac50bb2bd44ebd870a5a
SaneManiacMG <mmoroane@hotmail.com>
2023-09-04 11:48:27 +0200

    Implementing Runnable instead of extending it Overriding call()

--- com.sane.maniac.mg/threads/WorkerThreadExample.java
+++ com.sane.maniac.mg/threads/WorkerThreadExample.java
@@ -1,5 +1,7 @@
 package com.sane.maniac.mg.threads;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -8,32 +10,41 @@ import java.util.concurrent.Future;
 public class WorkerThreadExample {
 
   public static void main(String[] args) {
+    // Create thread pool
     ExecutorService executorService = Executors.newFixedThreadPool(3);
 
+    // Create a list of futures
+    List<Future<String>> futures = new ArrayList<>();
+
+    // Create 5 Runnable tasks and execute them
     for (int i = 1; i <= 5; i++) {
       RunnableTask task = new RunnableTask("Runnable Task " + i);
       executorService.execute(task);
     }
 
+    // Create 5 Callable tasks and submit them to the executor
     for (int i = 1; i <= 5; i++) {
       CallableTask task = new CallableTask("Callable Task " + i);
-
       Future<String> future = executorService.submit(task);
+      futures.add(future);
+    }
 
+    // Wait for all futures to complete
+    for (Future<String> future : futures) {
       try {
         String result = future.get();
-        System.out.println(result);
         System.out.println("Result from Callable Task: " + result);
       } catch (Exception e) {
         e.printStackTrace();
       }
     }
 
+    // Shutdown the executor
     executorService.shutdown();
   }
 }
 
-public class RunnableTask extends Runnable {
+class RunnableTask implements Runnable {
 
   private String name;
 
@@ -57,6 +68,7 @@ class CallableTask implements Callable<String> {
     this.name = name;
   }
 
+  @Override
   public String call() throws Exception {
     Thread.sleep(2000);
     return name + " is running on Thread: " + Thread.currentThread().getName();
